c062d22dcfe4d279c6b87c6a033ab29187986c3c
Javier Campos <j_campos.v@outlook.com>
2020-08-07 19:03:23 -0400

    Clase 5, ES7

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+//Buscar elemento en un array por su valor y no por index
+let numbers = [1,2,3,7,8];
+
+if (numbers.includes(9)) {
+    console.log('Si se encuentra el valor 7');
+} else {
+    console.log('No se encuentra');
+}
+
+//Elevar un numero a una potencia
+let base = 4;
+let exponent = 4;
+let result = base ** exponent;
+
+console.log(result);
