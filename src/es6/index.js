8f115643635fb92de66fcc83bf0fed199d0c6058
Javier Campos <j_campos.v@outlook.com>
2020-08-07 14:13:00 -0400

    Segunda clase

--- src/es6/index.js
+++ src/es6/index.js
@@ -23,3 +23,55 @@ console.log(epicPhrase);
 //es6
 let epicPhrase2 = `${hello} ${world}`
 console.log(epicPhrase2);
+
+
+let lorem = "Que pasa larvaaaa \n"
++ "otra frase.";
+
+//es6
+let lorem2 = `Otra frase
+ahora es otra frase`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
+let person = {
+    'name': 'Javier',
+    'age': 23,
+    'country': 'CL'
+}
+
+console.log(person.name, person.age, person.country);
+
+//es6
+let{ name, age, country } = person;
+console.log(name, age, country);
+
+
+//Arreglos
+let team1 = ['Javier', 'Julian', 'Ricardo'];
+let team2 = ['Valeria', 'Yesica', 'Camila'];
+
+let education  = ['David', ...team1, ...team2];
+
+console.log(education);
+
+
+//Las variables de tipo var son globales 
+//y las de tipo let solo son accesibles 
+//desde su mismo bloque de código
+{
+    var globalVar = 'Global Var';
+}
+{
+    let globalLet = 'Global Let';
+    console.log(globalLet);
+}
+
+console.log(globalVar);
+
+//No se puede modificar el valor de una constante
+const a = 'b';
+//a = 'a';
+console.log(a);
